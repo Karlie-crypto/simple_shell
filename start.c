8b712485d0966050def8a29b7a7b89d1da916bbf
Karlie-crypto <karlieemoyo@gmail.com>
2022-11-16 01:12:08 -0800

    Simple shell

---
+++ start.c
@@ -0,0 +1,14 @@
+#include "general.h"
+#include "main.h"
+#include "text.h"
+
+/**
+ * start - Handle the mode
+ * Description: Mode can be INTERACTIVE or NON_INTERACTIVE
+ *
+ * @info: Struct of information about the shell
+ **/
+void start(general_t *info)
+{
+	start_prompt(info);
+}
